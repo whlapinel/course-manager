6ad1eb3830b4e65c853a7842b657719b66301156
Will Lapinel <whlapinel@gmail.com>
2025-01-03 14:40:24 -0500

    before the slides change

--- temp_fix_slides/temp_fix_slides.go
+++ temp_fix_slides/temp_fix_slides.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"errors"
 	"gh_static_portfolio/cmd/data"
 	"gh_static_portfolio/cmd/domain"
+	"io/fs"
 	"log"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	queries, db, err := data.InitDB("../course_manager.db")
+	queries, db, err := data.InitDB("course_manager.db")
 	if err != nil {
-		log.Fatal()
+		log.Fatal(err)
 	}
 	defer db.Close()
 	courseRepo := data.NewCourseRepo(queries)
 	terms, err := courseRepo.GetTerms()
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, term := range terms {
 		courses, err := courseRepo.GetCourses(term.ID)
 		if err != nil {
@@ -37,6 +44,12 @@ func main() {
 
 				for _, lesson := range lessons {
 					srcPath := data.OldSlidesMarkdownFilePath(lesson)
+					_, err := os.Stat(srcPath)
+					if err != nil {
+						if errors.Is(err, fs.ErrNotExist) {
+							continue
+						}
+					}
 					slides := domain.NewSlides(lesson.Name, lesson.Description, srcPath)
 					_, err := courseRepo.SaveSlides(slides, lesson)
 					if err != nil {
