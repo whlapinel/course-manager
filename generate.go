6112658c9d01ac4a7564a28cb530145d84054211
Will Lapinel <whlapinel@gmail.com>
2025-02-01 09:00:07 -0500

    finish troubleshooting tailwind (output was pointing to wrong file)

--- generate.go
+++ generate.go
@@ -1,5 +1,6 @@
 package main
 
 //go:generate templ generate
-//go:generate npx tailwindcss -c tailwind-manager.config.js -i ./manager-input.css -o ./cmd/web_app/assets/dist/styles.css && npx tailwindcss -c tailwind-docs.config.js -i ./docs-input.css -o ./python/docs/styles/styles.css
+//go:generate npx tailwindcss -i ./manager-input.css -o ./internal/assets/dist/styles.css
+//go:npx tailwindcss -i ./docs-input.css -o ./python/docs/styles/styles.css
 //go:generate tsc --build
